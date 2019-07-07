fc414cfacb9b2c39898784b9617d0e00b56d932e
Jarod Aerts <jarod2017@gmail.com>
2019-07-06 22:53:45 -0500

    Made page to view HTML for article

---
+++ YoWiki/YoWiki/Views/ViewArticlePage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YoWiki.Views
+{
+    public partial class ViewArticlePage : ContentPage
+    {
+        public ViewArticlePage()
+        {
+            InitializeComponent();
+        }
+    }
+}
