f45c1895cd94eb7cca960b6da2d4444affa2e04a
JarodTAerts <jarod2017@gmail.com>
2019-07-12 21:35:17 -0500

    Added appcenter integration

--- YoWiki/YoWiki/App.xaml.cs
+++ YoWiki/YoWiki/App.xaml.cs
@@ -6,6 +6,9 @@ using YoWiki.Accessors.Interfaces;
 using YoWiki.Services;
 using YoWiki.Services.Interfaces;
 using YoWiki.Views;
+using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
 
 namespace YoWiki
 {
@@ -29,6 +32,10 @@ namespace YoWiki
         protected override void OnStart()
         {
             // Handle when your app starts
+            AppCenter.Start("android=97b563a9-317d-491a-973b-730e19c4f7c0;" +
+                  "uwp={Your UWP App secret here};" +
+                  "ios={Your iOS App secret here}",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
