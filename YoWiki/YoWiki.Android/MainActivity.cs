d1adef0c08969b61af0bcc17c0845c364c2fcfce
JarodTAerts <jarod2017@gmail.com>
2019-11-17 20:01:57 -0600

    Got the Icon working finally

--- YoWiki/YoWiki.Android/MainActivity.cs
+++ YoWiki/YoWiki.Android/MainActivity.cs
@@ -11,7 +11,7 @@ using Android.Content;
 
 namespace YoWiki.Droid
 {
-    [Activity(Label = "YoWiki", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "YoWiki", Icon = "@drawable/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
